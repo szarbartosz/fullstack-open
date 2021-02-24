9aa3909737880275a97b9488aba011780db59a95
szarbartosz <szarbartosz@gmail.com>
2021-02-24 23:48:46 +0100

    finished task 9.3

--- part9/ts-project/bmiCalculator.ts
+++ part9/ts-project/bmiCalculator.ts
@@ -1,23 +1,47 @@
-const calculateBmi = (height: number, mass: number) => {
-  const bmi: number = mass / Math.pow((height / 100), 2)
+interface BmiParameters {
+  height: number;
+  mass: number;
+}
 
-  if (bmi < 15) {
-    return 'Very severely underweight'
-  } else if (bmi >= 15 && bmi < 16) {
-    return 'Severely underweight'
-  } else if (bmi >= 16 && bmi < 18.5) {
-    return 'Underweight'
-  } else if (bmi >= 18.5 && bmi < 25) {
-    return 'Normal (healthy weight)'
-  } else if (bmi >= 25 && bmi < 30) {
-    return 'Overweight'
-  } else if (bmi >= 30 && bmi < 35) {
-    return 'Obese Class I (Moderately obese)'
-  } else if (bmi >= 35 && bmi < 40) {
-    return 'Obese Class II (Severely obese)'
-  } else if (bmi >= 40) {
-    return 'Obese Class III (Very severely obese)'
+const parseArguments = (args: Array<string>): BmiParameters => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      mass: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
   }
 }
 
-console.log(calculateBmi(198, 92))
+const calculateBmi = (height: number, mass: number): void => {
+  const bmi: number = mass / Math.pow((height / 100), 2)
+
+  if (bmi < 15) {
+    console.log('Very severely underweight');
+  } else if (bmi >= 15 && bmi < 16) {
+    console.log('Severely underweight');
+  } else if (bmi >= 16 && bmi < 18.5) {
+    console.log('Underweight');
+  } else if (bmi >= 18.5 && bmi < 25) {
+    console.log('Normal (healthy weight)');
+  } else if (bmi >= 25 && bmi < 30) {
+    console.log('Overweight');
+  } else if (bmi >= 30 && bmi < 35) {
+    console.log('Obese Class I (Moderately obese)');
+  } else if (bmi >= 35 && bmi < 40) {
+    console.log('Obese Class II (Severely obese)');   
+  } else if (bmi >= 40) {
+    console.log('Obese Class III (Very severely obese)');
+  }
+}
+
+try {
+  const { height, mass } = parseArguments(process.argv)
+  calculateBmi(height, mass)
+} catch (e) {
+  console.log('error occurred: ', e.message);
+}
---
+++ part9/ts-project/exerciseCalculator.ts
@@ -0,0 +1,77 @@
+interface TrainingDaysParameters {
+  training: number[];
+  target: number;
+}
+
+const parseTrainingArguments = (args: Array<string>): TrainingDaysParameters => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+
+  let correctDaysData: boolean = true;
+  for (let i = 3; i < args.length; i++) {    
+    if (isNaN(Number(args[i]))) {
+      correctDaysData = false
+    }
+  }
+  
+
+  if (!isNaN(Number(args[2])) && correctDaysData) {
+    let training: number[] = [];
+    for (let i = 3; i < args.length; i++) {
+      training = training.concat(Number(args[i]))
+    }
+    
+    return {
+      training: training,
+      target: Number(args[2])
+    }
+  } else {
+    throw new Error('Provided parameters are invalid!');
+  }
+}
+
+interface Result {
+  periodLength: number,
+  trainingDays: number,
+  success: boolean,
+  raiting: number,
+  raitingDescription: string,
+  target: number,
+  average: number
+}
+
+const calculateExcercises = (training: number[], target: number): void => {
+  const periodLength = training.length
+  const trainingDays = training.filter(t => t !== 0).length
+  const average = training.reduce((x, y) => x + y, 0) / periodLength
+  const success = average >= target
+
+  let raiting
+  let raitingDescription
+  if (average < 0.75 * target) {
+    raiting = 1
+    raitingDescription = 'poor performance'
+  } else if (average < target) {
+    raiting = 2
+    raitingDescription = 'not bad'
+  } else {
+    raiting = 3
+    raitingDescription = 'cool, goal reached!'
+  }  
+
+  console.log({
+    periodLength: periodLength,
+    trainingDays: trainingDays,
+    success: success,
+    raiting: raiting,
+    raitingDescription: raitingDescription,
+    target: target,
+    average: average
+  });
+}
+
+try {
+  const { training, target } = parseTrainingArguments(process.argv)
+  calculateExcercises(training, target)
+} catch (e) {
+  console.log('error occurred: ', e.message);
+}
